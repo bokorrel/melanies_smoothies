08ba2e012a2f63d2174b2eb2494b490d66901a7e
bokorrel <120737845+bokorrel@users.noreply.github.com>
2024-04-26 17:25:54 +0200

    Update streamlit_app.py
    convert request response to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,5 @@ if ingredients_list: #same, but shorter
         st.success('Your Smoothie is ordered, '+ name_on_order + '!', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response).json()
+st.dataframe(data=fruityvice_response.json(), use_container_width=True)
